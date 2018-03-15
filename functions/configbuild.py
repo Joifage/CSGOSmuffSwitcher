2024e8c9286f224501d6495b2f19be100057aa39
Joifage <leese92@hotmail.com>
2018-03-15 06:59:12 +0000

    Update configbuild.py

--- functions/configbuild.py
+++ functions/configbuild.py
@@ -11,12 +11,6 @@ def doesconfigexist():
         config = configparser.ConfigParser()
         config['general'] = {'csgo_path': "N/A",
                              'steam_path': "N/A"}
-        config['0'] = {'username': 'DUMMY',
-                       'lastuse': 'DUMMY',
-                       'rank': 'DUMMY',
-                       'prime': 'DUMMY',
-                       'active': 'DUMMY',
-                       'autolaunch': 'DUMMY'}
         with open('conf.ini', 'w') as configfile1:
             config.write(configfile1)
             configfile1.close()
